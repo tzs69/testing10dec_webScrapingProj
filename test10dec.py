a53d339966265aee2b70b83ac6c1971cf3b5d1f7
tzs69 <156649283+tzs69@users.noreply.github.com>
2024-12-10 23:15:15 +0800

    Update test10dec.py

--- test10dec.py
+++ test10dec.py
@@ -3,38 +3,34 @@ from pip._vendor import requests
 from bs4 import BeautifulSoup
 import re
 
-url = "xxx" #enter tapology top 50 fighters globally (specific weight class) link here
+url = "https://www.tapology.com/rankings/current-top-ten-light-heavyweight-mma-fighters-205-pounds"
 page = requests.get(url)
 
 soup = BeautifulSoup(page.text, "html.parser")
 
-body_tag = soup.find('body', {'data-controller': "sidebar responsive grow-textarea", 'data-location': "rankings.show"})
-
-general = body_tag.find('div', class_ = "contentWrap clearfix")
-content = general.find('div', id = "content")
-
-weightClassList = content.find('div', class_ = "pageHeading withSharing").find('h1').text.split(" ")
+weightClassList = soup.find('div', class_ = "pageHeading withSharing").find('h1').text.split(" ")
 if len(weightClassList) == 9: 
     weightClass = weightClassList[4] + " " + weightClassList[5]
 else: 
     weightClass = weightClassList[4]
 
-top50global_list = content.find('ul', class_ = "rankingItemsList")
-top50global = top50global_list.find_all('li', class_ = "rankingItemsItem")
+top50global = soup.find_all('li', class_ = "rankingItemsItem")
 
 total_Wins = 0
 total_Losses = 0
+total_Draws = 0
 total_NoContests = 0
 
 for fighter in top50global:
-    d0 = fighter.find('div', class_ = "rankingItemsItemRow name")
-    d1 = d0.find('h1', class_ = "right")
-    record = re.split("-|, ", d1.text)
+    winloss = fighter.find('h1', class_ = "right")
+    record = re.split("-|, | ", winloss.text)
     total_Wins += int(record[0])
     total_Losses += int(record[1])
-    total_NoContests += int(record[2])
+    total_Draws += int(record[2])
+    if len(record) != 3:
+        total_NoContests += int(record[3])
 
-print("The total win-loss-NC ratio for the top 50 %s globally is %i-%i-%i" % (weightClass, total_Wins, total_Losses, total_NoContests))
+print("The combined record of the top 50 %s globally is %i-%i-%i(Win-Loss-Draw) with %i No Contests" % (weightClass, total_Wins, total_Losses, total_Draws, total_NoContests))
 
 
 
